3ca5f7471749d166ca7d36e772beb31a4840c13f
pandzel <pandzel@esri.com>
2018-11-19 09:51:34 -0800

    Code formatting.

--- vesselshape-generator-processor/src/main/java/com/esri/geoevent/processor/vesselshapegenerator/VesselShapeGeneratorProcessor.java
+++ vesselshape-generator-processor/src/main/java/com/esri/geoevent/processor/vesselshapegenerator/VesselShapeGeneratorProcessor.java
@@ -21,7 +21,6 @@ package com.esri.geoevent.processor.vesselshapegenerator;
  * limitations under the License.
  * #L%
  */
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -41,219 +40,208 @@ import com.esri.geoevent.processor.vesselshapegenerator.provider.Provider;
 import com.esri.geoevent.processor.vesselshapegenerator.provider.ProviderException;
 
 public class VesselShapeGeneratorProcessor extends GeoEventProcessorBase {
-	private static final Log LOG = LogFactory.getLog(VesselShapeGeneratorProcessor.class);
-	//public GeoEventDefinitionManager manager;
+
+  private static final Log LOG = LogFactory.getLog(VesselShapeGeneratorProcessor.class);
+  //public GeoEventDefinitionManager manager;
   private final Provider shapeProvider;
-	private SpatialReference srIn;
-	private SpatialReference srBuffer;
-	private SpatialReference srOut;
-	private String rangeSource;
-	private double rangeConstant;
-	private String rangeEventFld;
-	private String rangeUnits;
-	private String bearingSource;
-	private double bearingConstant;
-	private String bearingEventFld;
-	private String traversalSource;
-	private double traversalConstant;
-	private String traversalEventFld;
-	private int inwkid;
-	private int outwkid;
-	private int bufferwkid;
-	private String geosrc = "";
-	private String geometryEventFld;
-	private String xfield;
-	private String yfield;
+  private SpatialReference srIn;
+  private SpatialReference srBuffer;
+  private SpatialReference srOut;
+  private String rangeSource;
+  private double rangeConstant;
+  private String rangeEventFld;
+  private String rangeUnits;
+  private String bearingSource;
+  private double bearingConstant;
+  private String bearingEventFld;
+  private String traversalSource;
+  private double traversalConstant;
+  private String traversalEventFld;
+  private int inwkid;
+  private int outwkid;
+  private int bufferwkid;
+  private String geosrc = "";
+  private String geometryEventFld;
+  private String xfield;
+  private String yfield;
 
-	public VesselShapeGeneratorProcessor(GeoEventProcessorDefinition definition, Provider shapeProvider) throws ComponentException {
-		super(definition);
+  public VesselShapeGeneratorProcessor(GeoEventProcessorDefinition definition, Provider shapeProvider) throws ComponentException {
+    super(definition);
     this.shapeProvider = shapeProvider;
-	}
+  }
 
-	
-	@Override
-	public boolean isGeoEventMutator() {
-		return true;
-	}
+  @Override
+  public boolean isGeoEventMutator() {
+    return true;
+  }
 
-	@Override
-	public void afterPropertiesSet() {
-		try {
-			rangeSource = properties.get("rangeSource").getValue().toString();
-			rangeConstant = (Double) properties.get("range").getValue();
-			rangeEventFld = properties.get("rangeEvent").getValue().toString();
-			rangeUnits = properties.get("units").getValue().toString();
+  @Override
+  public void afterPropertiesSet() {
+    try {
+      rangeSource = properties.get("rangeSource").getValue().toString();
+      rangeConstant = (Double) properties.get("range").getValue();
+      rangeEventFld = properties.get("rangeEvent").getValue().toString();
+      rangeUnits = properties.get("units").getValue().toString();
 
-			bearingSource = properties.get("bearingSource").getValue()
-					.toString();
-			bearingConstant = (Double) properties.get("bearing").getValue();
-			bearingEventFld = properties.get("bearingEvent").getValue()
-					.toString();
+      bearingSource = properties.get("bearingSource").getValue().toString();
+      bearingConstant = (Double) properties.get("bearing").getValue();
+      bearingEventFld = properties.get("bearingEvent").getValue().toString();
 
-			traversalSource = properties.get("traversalSource").getValue()
-					.toString();
-			traversalConstant = (Double) properties.get("traversal").getValue();
-			traversalEventFld = properties.get("traversalEvent").getValue()
-					.toString();
-			//inwkid = (Integer) properties.get("wkidin").getValue();
-			outwkid = (Integer) properties.get("wkidout").getValue();
-			bufferwkid = (Integer) properties.get("wkidbuffer").getValue();
+      traversalSource = properties.get("traversalSource").getValue().toString();
+      traversalConstant = (Double) properties.get("traversal").getValue();
+      traversalEventFld = properties.get("traversalEvent").getValue().toString();
+      
+      outwkid = (Integer) properties.get("wkidout").getValue();
+      bufferwkid = (Integer) properties.get("wkidbuffer").getValue();
 
-			geosrc = properties.get("geosrc").getValueAsString();
-			geometryEventFld = properties.get("geoeventfld").getValue()
-					.toString();
-			xfield = properties.get("xfield").getValueAsString();
-			yfield = properties.get("yfield").getValueAsString();
-		} catch (Exception e) {
-			LOG.error(e.getMessage());
-		}
+      geosrc = properties.get("geosrc").getValueAsString();
+      geometryEventFld = properties.get("geoeventfld").getValue().toString();
+      xfield = properties.get("xfield").getValueAsString();
+      yfield = properties.get("yfield").getValueAsString();
+    } catch (Exception e) {
+      LOG.error(e.getMessage());
+    }
 
-	}
+  }
 
-	@Override
-	public void validate() throws ValidationException {
-		super.validate();
+  @Override
+  public void validate() throws ValidationException {
+    super.validate();
 
-		if (rangeConstant <= 0) {
-			throw new ValidationException(
-					"A constant range must be greater than 0");
-		}
+    if (rangeConstant <= 0) {
+      throw new ValidationException(
+              "A constant range must be greater than 0");
+    }
 
-		if (bearingConstant < 0 || bearingConstant >= 360) {
-			throw new ValidationException(
-					"A constant bearing must be >= 0 and < 360");
-		}
+    if (bearingConstant < 0 || bearingConstant >= 360) {
+      throw new ValidationException(
+              "A constant bearing must be >= 0 and < 360");
+    }
 
-		if (traversalConstant < 0 || bearingConstant >= 360) {
-			throw new ValidationException(
-					"A constant traversal must be > 0 and < 360");
-		}
+    if (traversalConstant < 0 || bearingConstant >= 360) {
+      throw new ValidationException(
+              "A constant traversal must be > 0 and < 360");
+    }
 
-		
+    try {
+      srBuffer = SpatialReference.create(bufferwkid);
+    } catch (Exception e) {
+      throw new ValidationException(
+              "The spatial processing wkid is invalid");
+    }
 
-		try {
-			srBuffer = SpatialReference.create(bufferwkid);
-		} catch (Exception e) {
-			throw new ValidationException(
-					"The spatial processing wkid is invalid");
-		}
+    try {
+      srOut = SpatialReference.create(outwkid);
+    } catch (Exception e) {
+      throw new ValidationException("The output wkid is invalid");
+    }
+  }
 
-		try {
-			srOut = SpatialReference.create(outwkid);
-		} catch (Exception e) {
-			throw new ValidationException("The output wkid is invalid");
-		}
-	}
+  @Override
+  public GeoEvent process(GeoEvent ge) throws Exception {
 
-	@Override
-	public GeoEvent process(GeoEvent ge) throws Exception {
+    try {
+      if (!ge.getGeoEventDefinition().getTagNames().contains("GEOMETRY")) {
+        return null;
+      }
+      srIn = ge.getGeometry().getSpatialReference();
+      inwkid = srIn.getID();
 
-		try {
-			if(!ge.getGeoEventDefinition().getTagNames().contains("GEOMETRY"))
-			{
-				return null;
-			}
-			srIn=ge.getGeometry().getSpatialReference();	
-			inwkid = srIn.getID();
-			double range;
-			if (rangeSource.equals("Constant")) {
-				range = rangeConstant;
-			} else {
-				range = (Double) ge.getField(rangeEventFld);
-			}
+      // vessel length
+      double range;
+      if (rangeSource.equals("Constant")) {
+        range = rangeConstant;
+      } else {
+        range = (Double) ge.getField(rangeEventFld);
+      }
 
-			double bearing;
+      // bearing
+      double bearing;
 
-			if (bearingSource.equals("Constant")) {
-				bearing = bearingConstant;
-			} else {
-				bearing = (Double) ge.getField(bearingEventFld);
-			}
+      if (bearingSource.equals("Constant")) {
+        bearing = bearingConstant;
+      } else {
+        bearing = (Double) ge.getField(bearingEventFld);
+      }
 
-			double traversal;
+      // vessel width
+      double traversal;
 
-			if (traversalSource.equals("Constant")) {
-				traversal = traversalConstant;
-			} else {
-				traversal = (Double) ge.getField(traversalEventFld);
-			}
+      if (traversalSource.equals("Constant")) {
+        traversal = traversalConstant;
+      } else {
+        traversal = (Double) ge.getField(traversalEventFld);
+      }
 
-			Point originGeo = null;
-			if (geosrc.equals("event")) {
-				MapGeometry mapGeo = ge.getGeometry();
-				originGeo = (Point) mapGeo.getGeometry();
-			}
-			if (geosrc.equals("geodef")) {
-				originGeo = (Point) ge.getField(geometryEventFld);
-			}
-			if (geosrc.equals("coord")) {
-				Double ox = (Double) ge.getField(xfield);
-				Double oy = (Double) ge.getField(yfield);
-				originGeo = new Point(ox, oy, inwkid);
-			}
-			double x = originGeo.getX();
-			double y = originGeo.getY();
-			
-			//Geometry vesselShape = constructRangeFan(x, y, range, rangeUnits, bearing,
-			//		traversal);
+      // vessel position
+      Point originGeo = null;
+      if (geosrc.equals("event")) {
+        MapGeometry mapGeo = ge.getGeometry();
+        originGeo = (Point) mapGeo.getGeometry();
+      }
+      if (geosrc.equals("geodef")) {
+        originGeo = (Point) ge.getField(geometryEventFld);
+      }
+      if (geosrc.equals("coord")) {
+        Double ox = (Double) ge.getField(xfield);
+        Double oy = (Double) ge.getField(yfield);
+        originGeo = new Point(ox, oy, inwkid);
+      }
 
-			
-			Point centerProj = (Point) GeometryEngine.project(originGeo, srIn,
-					srBuffer);
+      Point centerProj = (Point) GeometryEngine.project(originGeo, srIn, srBuffer);
 
       double shipLength = range;
-			double shipWidth = traversal;
+      double shipWidth = traversal;
       Shape shapeDefinition = getShape(ge);
-      
-			Geometry vesselShape = GeometryUtility.generateVesselShape(centerProj, shipWidth, shipLength, bearing, shapeDefinition);
-			
-			Geometry vesselShapeOut = GeometryEngine.project(vesselShape, srBuffer, srOut);			
-			MapGeometry outMapGeo = new MapGeometry(vesselShapeOut, srOut);
-			ge.setGeometry(outMapGeo);
-			return ge;
-		} catch (Exception e) {
-			LOG.error(e.getMessage());
-			throw e;
-		}
-	}
-  
+
+      Geometry vesselShape = GeometryUtility.generateVesselShape(centerProj, shipWidth, shipLength, bearing, shapeDefinition);
+
+      Geometry vesselShapeOut = GeometryEngine.project(vesselShape, srBuffer, srOut);
+      MapGeometry outMapGeo = new MapGeometry(vesselShapeOut, srOut);
+      ge.setGeometry(outMapGeo);
+      return ge;
+    } catch (Exception e) {
+      LOG.error(e.getMessage());
+      throw e;
+    }
+  }
+
   private Shape getShape(GeoEvent ge) throws ProviderException {
     // TODO: implement reading finding a right shape by an attribute from ge
     return shapeProvider.readShapes().get("default");
   }
 
-	private Geometry constructRangeFan(double x, double y, double range,
-			String unit, double bearing, double traversal) throws Exception {
-		try {
-			Polygon fan = new Polygon();
-			Point center = new Point();
-			center.setX(x);
-			center.setY(y);
+  private Geometry constructRangeFan(double x, double y, double range,
+          String unit, double bearing, double traversal) throws Exception {
+    try {
+      Polygon fan = new Polygon();
+      Point center = new Point();
+      center.setX(x);
+      center.setY(y);
 
-			Point centerProj = (Point) GeometryEngine.project(center, srIn,
-					srBuffer);
+      Point centerProj = (Point) GeometryEngine.project(center, srIn, srBuffer);
 
-			double centerX = centerProj.getX();
-			double centerY = centerProj.getY();
-			bearing = GeometryUtility.Geo2Arithmetic(bearing);
-			double leftAngle = bearing - (traversal / 2);
-			double rightAngle = bearing + (traversal / 2);
-			int count = (int) Math.round(Math.abs(leftAngle - rightAngle));
-			fan.startPath(centerProj);
-			UnitConverter uc = new UnitConverter();
-			range = uc.Convert(range, unit, srBuffer);
-			for (int i = 0; i < count; ++i) {
-				double d = Math.toRadians(leftAngle + i);
-				double arcX = centerX + (range * Math.cos(d));
-				double arcY = centerY + (range * Math.sin(d));
-				Point arcPt = new Point(arcX, arcY);
-				fan.lineTo(arcPt);
-			}
-			fan.closeAllPaths();
-			return fan;
-		} catch (Exception e) {
-			LOG.error(e.getMessage());
-			throw e;
-		}
-	}
+      double centerX = centerProj.getX();
+      double centerY = centerProj.getY();
+      bearing = GeometryUtility.Geo2Arithmetic(bearing);
+      double leftAngle = bearing - (traversal / 2);
+      double rightAngle = bearing + (traversal / 2);
+      int count = (int) Math.round(Math.abs(leftAngle - rightAngle));
+      fan.startPath(centerProj);
+      UnitConverter uc = new UnitConverter();
+      range = uc.Convert(range, unit, srBuffer);
+      for (int i = 0; i < count; ++i) {
+        double d = Math.toRadians(leftAngle + i);
+        double arcX = centerX + (range * Math.cos(d));
+        double arcY = centerY + (range * Math.sin(d));
+        Point arcPt = new Point(arcX, arcY);
+        fan.lineTo(arcPt);
+      }
+      fan.closeAllPaths();
+      return fan;
+    } catch (Exception e) {
+      LOG.error(e.getMessage());
+      throw e;
+    }
+  }
 }
